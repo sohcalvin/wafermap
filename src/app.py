8e5a98fd466bfa1e3582b66e2f6f47a6e7b9e500
Calvin Soh <calvin.soh@sap.com>
2016-01-28 15:04:44 +0800

    Clustering map implementation

--- src/app.py
+++ src/app.py
@@ -1,43 +1,11 @@
 import pandas as pd
 import numpy as np
+from common.data_utility import generateData
+from common.data_utility import loadData
 
 from models.wafermap import WaferMap
 import os
 import json
-
-dataDir = "data/"
-
-def generateData() :
-    # pattern1 = [[i,i] for i in (range(25,40))]
-    pattern1 = [[i,i] for i in (range(25,35))]
-    pattern1.extend(pattern1)
-    pattern2 = [[i,i] for i in (range(26,40))]
-    pattern2.extend(pattern2)
-    for i in range(1,200):
-        waferid = "wafer{0}".format(i)
-        waferMap = WaferMap(waferid)
-        # waferMap.initMap(100,100,pattern1)
-        if(np.random.rand() > 0.5) :
-            waferMap.initMap(100,100,pattern1)
-            waferMap.setTitle("Pattern1")
-        else :
-            waferMap.initMap(100,100,pattern2)
-            waferMap.setTitle("Pattern2")
-        waferMap.save(dataDir + "{0}".format(waferid))
-
-
-
-def loadData():
-    outData = []
-    for file_name in os.listdir(dataDir):
-        waferMap = WaferMap.load(os.path.join(dataDir,file_name))
-        bodyArray = waferMap.getBody()
-        bodyOneLine = []
-        for i in bodyArray :
-            bodyOneLine.extend(i)
-        outData.append([waferMap.getTitle(), bodyOneLine])
-
-    return outData
 
 def buildNaiveBayesModel(X,Y) :
     #Naive Bayes Classifier
@@ -46,15 +14,12 @@ def buildNaiveBayesModel(X,Y) :
     classifier.fit(X,Y)
     return classifier
 
-generateData()
+# generateData()
 
 dataOneLinePerMap = loadData()
 tmp = [i[1] for i in dataOneLinePerMap]
-# print(tmp)
 X = pd.DataFrame(tmp)
 Y = np.array([i[0] for i in dataOneLinePerMap])
-
-
 
 
 from sklearn.cross_validation import train_test_split
