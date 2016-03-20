5e6846cb04adb7987c8b847b0314f66fe87ad6d3
Calvin <soh_calvin@yahoo.com>
2016-03-20 23:41:01 +0800

    drag and print coord

--- src/webapp/frontend/scripts/directives/wafermapeditor.js
+++ src/webapp/frontend/scripts/directives/wafermapeditor.js
@@ -45,31 +45,49 @@ svgApp.directive("wafermapEditor", function($parse,$window) {
                             .attr("cx",centerX)
                             .attr("cy",centerY)
                             .attr("r",radius)
-                            //.attr("fill","white");
                             .attr("fill","lightblue");
-
-              canvas
-              .selectAll("line").data([20,21,22,23,24,25,26,27,28,29]).enter()
+              var xTicks = d3.range(chipSizeX, canvasSizeX, chipSizeX);
+              var yTicks = d3.range(chipSizeY, canvasSizeY, chipSizeY);
+              var gridStroke = "gray";
+              var gridStrokeWidth = 0.5;
+              canvas.selectAll(".yline").data(yTicks)
+                 .enter()
                  .append("line")
                  .attr("x1", 0)
                  .attr("y1", function(d){ return d;})
                  .attr("x2", canvasSizeX)
                  .attr("y2", function(d){ return d;})
-                 .attr("stroke-width", 1)
-                 .attr("stroke", "black");
-              canvas
-              .selectAll("line").data([5], function(d){  return d;}).enter()
+                 .attr("stroke-width", gridStrokeWidth)
+                 .attr("stroke", gridStroke);
+              canvas.selectAll(".xline").data(xTicks)
+                 .enter()
                  .append("line")
-                 .attr("x1", 0)
-                 .attr("y1", function(d){ return d;})
-                 .attr("x2", canvasSizeX)
-                 .attr("y2", function(d){ return d;})
-                 .attr("stroke-width", 1)
-                 .attr("stroke", "black");
+                 .attr("x1", function(d){ return d;})
+                 .attr("y1", 0)
+                 .attr("x2", function(d){ return d;})
+                 .attr("y2", canvasSizeY)
+                 .attr("stroke-width", gridStrokeWidth)
+                 .attr("stroke", gridStroke);
 
 
-          }
+                var drag = d3.behavior.drag();
+                canvas.call(drag);
+                drag.on("drag", mousemove);
+    //             canvas.on("mousemove", mousemove);
+                function mousemove(d, i) {
+                        posXY = d3.mouse(this);
+                        cellXY = positionToCell(posXY[0],posXY[1]);
+                        console.log(cellXY);
 
+                }
+
+                function positionToCell(xPos, yPos){
+                    var cellXY = [];
+                    cellXY[0] = Math.floor(xPos / chipSizeX);
+                    cellXY[1] = Math.floor(yPos / chipSizeY);
+                    return cellXY;
+                }
+            }
             function drawWaferMap(){
                 if(!wafermap_data){
                     console.log("Mapdata at " + attrs.mapData + "  not ready");
